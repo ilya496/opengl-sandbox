dd80d4e2410647c8c12358cb7269665d4564fd50
Ilja Novak <ilya496>
2025-05-25 22:36:32 +0200

    basic vec2

--- main.cpp
+++ main.cpp
@@ -4,15 +4,20 @@
 #include "Shader.h"
 #include "GLContext.h"
 #include "Math.h"
+#include <iostream>
 
 static auto gStartTime = std::chrono::high_resolution_clock::now();
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
+//int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) 
+int main()
+{
+
     Window window(L"GL Window", 800, 600);
 
     HDC hdc = window.GetHDC();
     HGLRC hglrc = CreateOpenGLContext(hdc);
-    if (!hglrc) {
+    if (!hglrc) 
+    {
         MessageBox(NULL, L"Failed to create OpenGL 4.1 context.", L"Error", MB_OK | MB_ICONERROR);
         return -1;
     }
@@ -22,7 +27,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
     GLuint vbo;
     Shader shader = LoadShaderFromFile("basic.vert", "basic.frag");
 
-    float diamond[] = {
+    float diamond[] = 
+    {
          0.0f,  0.0f,  // center
          0.0f,  0.75f, // top
          0.25f, 0.0f,  // right
@@ -61,6 +67,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
         height = max(height, 1);
         glViewport(0, 0, width, height);
         });
+
+    math::vec2 v1(1.0f, 2.5f), v2(3.0f, 4.0f);
+    math::vec2 result = v1 + v2;
+    std::cout << result << std::endl;
 
     window.Run();
     return 0;
